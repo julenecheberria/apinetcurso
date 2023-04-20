b1c75edfba732cf5f46b84b3c2b61f82c1d3b73b
julenecheberria <julen.echeberria@gmail.com>
2023-04-20 09:12:33 +0200

    añadido producto

--- apinetcurso/Controllers/ProductsController.cs
+++ apinetcurso/Controllers/ProductsController.cs
@@ -15,7 +15,8 @@ namespace ProductsApp.Controllers
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Puzzle", Category = "Toys", Price = 2 }
         };
 
         public IEnumerable<Product> GetAllProducts()
