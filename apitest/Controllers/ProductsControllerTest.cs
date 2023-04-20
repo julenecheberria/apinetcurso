c30b1c5000de1505acb39885bda5eb5e655e3e2e
julenecheberria <julen.echeberria@gmail.com>
2023-04-20 10:38:18 +0200

    test ghungos

--- apitest/Controllers/ProductsControllerTest.cs
+++ apitest/Controllers/ProductsControllerTest.cs
@@ -3,8 +3,10 @@ using ProductsApp.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using System.Text;
 using System.Threading.Tasks;
+using apinetcurso.Models;
 
 namespace ProductsApp.Controllers.Tests
 {
@@ -14,14 +16,27 @@ namespace ProductsApp.Controllers.Tests
         [TestMethod()]
         public void GetAllProductsTest()
         {
-            Assert.IsTrue(true);
+
+            var controlador = new ProductsController();
+
+            var result = controlador.GetAllProducts() as List<Product>;
+
+            Assert.IsTrue(result.Count == 4 );
+            Assert.IsFalse(result.Count == 5);
+            
             //Assert.Fail();
         }
 
         [TestMethod()]
         public void GetProductTest()
         {
-            Assert.IsTrue(true);
+            var controlador = new ProductsController();
+
+            var result = controlador.GetProduct(2) as Product;
+
+            Assert.AreEqual(result.Name,"Yo-yo");
         }
+
+
     }
 }
